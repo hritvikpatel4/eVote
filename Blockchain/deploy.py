891e1084449476436742976b287b61ff385c81b7
Hritvik Patel <hritvik.patel4@gmail.com>
2021-02-14 10:54:08 +0530

    Changed log paths in deploy script

--- Blockchain/deploy.py
+++ Blockchain/deploy.py
@@ -14,10 +14,10 @@ def spawnContainer(type_of_container, orderer_flag, container_number):
         container_name = "orderer" + str(container_number)
     
     if type_of_container == "load_balancer":
-        container = client.containers.run(image=image_to_run, detach=True, network="blockchain", name=container_name, hostname=container_name, ports={'8080/tcp': 80}, environment=["CUSTOM_PORT=8080"], volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}, '/home/$HOME/logs/': {'bind': '/usr/src/app/logs', 'mode': 'rw'}})
+        container = client.containers.run(image=image_to_run, detach=True, network="blockchain", name=container_name, hostname=container_name, ports={'8080/tcp': 80}, environment=["CUSTOM_PORT=8080"], volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}, '/home/blockchain/logs/': {'bind': '/usr/src/app/logs', 'mode': 'rw'}})
 
     else:
-        container = client.containers.run(image=image_to_run, detach=True, network="blockchain", name=container_name, hostname=container_name, environment=["CUSTOM_PORT=80"], volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}, '/home/$HOME/logs/': {'bind': '/usr/src/app/logs', 'mode': 'rw'}})
+        container = client.containers.run(image=image_to_run, detach=True, network="blockchain", name=container_name, hostname=container_name, environment=["CUSTOM_PORT=80"], volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}, '/home/blockchain/logs/': {'bind': '/usr/src/app/logs', 'mode': 'rw'}})
     
     time.sleep(10)
     process = container.top()
