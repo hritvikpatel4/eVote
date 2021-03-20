49240bc630f870b86acc5440f85e6943adb7bfb3
Hritvik Patel <hritvik.patel4@gmail.com>
2021-03-20 19:42:27 +0530

    pushing changes

--- get_results.py
+++ get_results.py
@@ -9,8 +9,6 @@ ip_list = []
 
 for i in range(len(json_data)):
     if "hbc" in json_data[i]["name"]:
-        print(json_data[i]["name"])
-        print(json_data[i]["networkInterfaces"][0]["accessConfigs"][0]["natIP"])
         ip_list.append(json_data[i]["networkInterfaces"][0]["accessConfigs"][0]["natIP"])
 
 result = []
@@ -26,6 +24,21 @@ for i in range(1, len(result)):
     for key in result[i].keys():
         temp_result[key] += result[i][key]
 
-final_result = dict(sorted(temp_result.items(), key=lambda item: item[1]))
+final_result = sorted(temp_result.items(), key=lambda item: -item[1])
 
-print(json.dumps(final_result, indent=4)
+print("\n\tWinners:")
+
+if final_result[0] > final_result[1]:
+    print("\t\t{} with a total of {} votes".format(final_result[0][0], final_result[0][1]))
+
+else:
+    print("\t\t{} with a total of {} votes".format(final_result[0][0], final_result[0][1]))
+    try:
+        i = 1
+        while final_result[0] == final_result[i]:
+            print("\t\t{} with a total of {} votes".format(final_result[i][0], final_result[i][1]))
+            i += 1
+    
+    except IndexError:
+        pass
+print()
